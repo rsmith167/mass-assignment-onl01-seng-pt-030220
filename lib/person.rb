31eb502b7dc56c37008bb5d0f8036a1fd9949420
Rwanda S <rwanda.ace@gmail.com>
2020-04-09 17:28:16 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,10 @@
 class Person
   #your code here
-  attr_accessor
+  attr_accessor :name, :birthday, :hair_color, :eye_color, :height, 
+:weight, :handed, :complexion, :t_shirt_size, 
+:wrist_size, :glove_size, :pant_length, :pant_width
+def initialize(attributes)
+  attributes.each {|k,v| self.send("#{k}=",v)}
+end
   
 end
